24e36121250dfa2439ea68c8f79f0c81b69c819c
ayan8653962 <ayan8653962@gmail.com>
2025-11-23 02:03:07 +0500

    practice

--- app.js
+++ app.js
@@ -1,15 +1,40 @@
 alert("WellCome Our Website")
+let fname = document.getElementById("firstname") 
+let lname = document.getElementById("lastname") 
+let emaildata = document.getElementById("email")
+let pnumber = document.getElementById("number")
 function inputhandel(){
-    let input = document.getElementsByClassName("inputname")[0].value
-    if(input == ""){
-        document.write("Enter First Name ")
+    if(fname.value == ""){
+        console.log("Enter First name")
     }
-    else if(input.length > 3){
-        document.write("Enter Correct Name ")
+    else if(fname.value.length < 3){
+        console.log("Enter Correct Name ")
     }
-    else{
-        document.write("Invalid Name")
+    else if(lname.value == ""){
+        console.log("Enter Last name")
     }
+    else if(lname.value.length < 3){
+        console.log("Enter Correct Name ")
+    }
+    else if(emaildata == ""){
+     console.log("Enter Email Adderess")
+    }
+    else if(!emaildata.value.includes("@")){
+     console.log("Enter Correct Email Adderess")
+    }
+    else if(pnumber.value == ""){
+     console.log("Enter Phone Number")
+    }
+    else if(isNaN(pnumber.value)){
+     console.log("Please Enter Only Number !")
+    }
+    else if(pnumber.value.length == 12){
+    console.log("Please Enter Only 11 Digit Number")
+    }
+   else if(pnumber.value.length < 11){
+    console.log("Please Enter an 11 Digit Number ")    
+   }
+
 }
-inputhandel()
+
 
